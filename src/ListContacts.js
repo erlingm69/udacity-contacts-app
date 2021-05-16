0f49091c5bcfed7652b7ed23a5c54b9b9aaf4ff8
Erling Mårtensson <erling.martensson@gmail.com>
2021-05-16 12:42:43 +0200

    Pass Data

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,8 +4,13 @@ export default class ListContacts extends Component {
   render() {
     console.log("Props", this.props)
     return (
-      <ol className="contact-list">
-
+      <ol className="contact-list">{
+        this.props.contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))
+      }
       </ol>
     )
   }
